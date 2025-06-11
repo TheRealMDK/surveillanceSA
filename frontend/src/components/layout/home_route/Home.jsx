d4ea83cc4a6505b5d0c38ba40976fc925a98bb75
Clinton Oliver <devbyclinton@gmail.com>
2025-06-11 19:41:34 +0200

    update landing

--- frontend/src/components/layout/home_route/Home.jsx
+++ frontend/src/components/layout/home_route/Home.jsx
@@ -12,36 +12,40 @@ const Home = () => {
     <>
       <NavBar
         content={
-          <div className="flex flex-col">
+          <div className="flex flex-col relative">
+            <div className="absolute inset-0 w-full h-full -z-20 bg-clip-padding bg-[linear-gradient(#009a99_1.4px,_transparent_1.4px),linear-gradient(to_right,_#009a99_1.4px,_transparent_1.4px)] bg-[size:60px_60px]"></div>
+            <div className="absolute inset-0 w-full h-full -z-10 bg-white/20 dark:bg-base-300/20 backdrop-blur-xs dark:backdrop-blur-sm"></div>
             <div className="h-[64px]"></div>
             <Element name="landing" className="active">
               <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
                 <Landing />
               </div>
             </Element>
-            <Element name="about" className="">
-              <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
-                <About />
-              </div>
-            </Element>
-            <Element name="services" className="">
-              <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
-                <Services />
-              </div>
-            </Element>
-            <Element name="gallery" className="">
-              <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
-                <Gallery />
-              </div>
-            </Element>
-            <Element name="contact" className="">
-              <div
-                id="contact"
-                className="md:h-[calc(100dvh_-_64px)] flex items-center justify-center"
-              >
-                <ContactUs />
-              </div>
-            </Element>
+            <div className="bg-base-200">
+              <Element name="about" className="">
+                <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
+                  <About />
+                </div>
+              </Element>
+              <Element name="services" className="">
+                <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
+                  <Services />
+                </div>
+              </Element>
+              <Element name="gallery" className="">
+                <div className="h-[calc(100dvh_-_64px)] flex items-center justify-center">
+                  <Gallery />
+                </div>
+              </Element>
+              <Element name="contact" className="">
+                <div
+                  id="contact"
+                  className="md:h-[calc(100dvh_-_64px)] flex items-center justify-center"
+                >
+                  <ContactUs />
+                </div>
+              </Element>
+            </div>
           </div>
         }
       />
