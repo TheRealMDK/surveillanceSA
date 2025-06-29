ee1edb4c05e421a535a975af981ae089415cc859
Clinton Oliver <clintonoliver25@gmail.com>
2025-06-29 17:39:06 +0200

    continue services section

--- frontend/src/components/layout/home_route/sections/Services.jsx
+++ frontend/src/components/layout/home_route/sections/Services.jsx
@@ -4,39 +4,102 @@ const Services = () => {
       {/* name of each tab group should be unique */}
       <div className="w-[calc(100%_-_2rem)] md:w-[80vw] h-[calc(100dvh_-_150px)] flex items-center justify-center">
         <div className="tabs tabs-lift w-full">
-          <input
-            type="radio"
-            name="my_tabs_3"
-            className="tab"
-            aria-label="Tab 1"
-          />
-          <div className="tab-content min-h-[calc(100dvh_-_150px)] bg-base-100 border-base-300 p-6 md:bg-[url('/service1ll.png')] dark:md:bg-[url('/service1ld.png')] bg-cover bg-center">
-            <div className="w-full h-full flex justify-center items-center">
+          <label className="tab has-[input:checked]:text-cyber_teal">
+            <input className="" type="radio" name="my_tabs_4" defaultChecked />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-4 me-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+              />
+            </svg>
+            Tab 1
+          </label>
+          <div className="tab-content h-[calc(100dvh_-_150px_-_40px)] bg-base-100 border-base-300 p-6 md:bg-[url('/services/desktop/service1ll.png')] dark:md:bg-[url('/services/desktop/service1ld.png')] bg-cover bg-center bg-no-repeat bg-clip-border">
+            <div className="w-full h-[calc(100dvh_-_150px_-_40px)]  flex justify-center items-center">
               Tab content 1
             </div>
           </div>
 
-          <input
-            type="radio"
-            name="my_tabs_3"
-            className="tab"
-            aria-label="Tab 2"
-            defaultChecked
-          />
-          <div className="tab-content min-h-[calc(100dvh_-_150px)] bg-base-100 border-base-300 p-6">
-            <div className="flex justify-center items-center h-full">
+          <label className="tab has-[input:checked]:text-cyber_teal">
+            <input className="" type="radio" name="my_tabs_4" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-4 me-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+              />
+            </svg>
+            Tab 2
+          </label>
+
+          <div className="tab-content h-[calc(100dvh_-_150px_-_40px)] bg-base-100 border-base-300 p-6 md:bg-[url('/services/desktop/service1ll.png')] dark:md:bg-[url('/services/desktop/service1ld.png')] bg-cover bg-center bg-no-repeat bg-clip-border">
+            <div className="w-full h-[calc(100dvh_-_150px_-_40px)]  flex justify-center items-center">
               Tab content 2
             </div>
           </div>
-          <input
-            type="radio"
-            name="my_tabs_3"
-            className="tab"
-            aria-label="Tab 3"
-          />
-          <div className="tab-content min-h-[calc(100dvh_-_150px)] bg-base-100 border-base-300 p-6">
-            <div className="flex justify-center items-center h-full">
+
+          <label className="tab has-[input:checked]:text-cyber_teal">
+            <input className="" type="radio" name="my_tabs_4" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-4 me-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+              />
+            </svg>
+            Tab 3
+          </label>
+
+          <div className="tab-content h-[calc(100dvh_-_150px_-_40px)] bg-base-100 border-base-300 p-6 md:bg-[url('/services/desktop/service1ll.png')] dark:md:bg-[url('/services/desktop/service1ld.png')] bg-cover bg-center bg-no-repeat bg-clip-border">
+            <div className="w-full h-[calc(100dvh_-_150px_-_40px)]  flex justify-center items-center">
               Tab content 3
+            </div>
+          </div>
+
+          <label className="tab has-[input:checked]:text-cyber_teal">
+            <input className="" type="radio" name="my_tabs_4" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-4 me-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z"
+              />
+            </svg>
+            Tab 4
+          </label>
+
+          <div className="tab-content h-[calc(100dvh_-_150px_-_40px)] bg-base-100 border-base-300 p-6 md:bg-[url('/services/desktop/service1ll.png')] dark:md:bg-[url('/services/desktop/service1ld.png')] bg-cover bg-center bg-no-repeat bg-clip-border">
+            <div className="w-full h-[calc(100dvh_-_150px_-_40px)]  flex justify-center items-center">
+              Tab content 4
             </div>
           </div>
         </div>
